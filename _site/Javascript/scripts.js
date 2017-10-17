1631faa9711115059eec7c5d6975d9947b7bbac3
katieziz <katie_zizian@hotmail.com>
2017-10-17 10:58:00 +0800

    JQuery smooth scroll
    smooth scroll

---
+++ _site/Javascript/scripts.js
@@ -0,0 +1,35 @@
+//JQuery goes here
+$( document ).ready(function() {
+
+}); //This closes your JQuery
+//SMOOTH SCROLL JQuery
+function scrollToSection(event) {
+
+  // Log
+  console.log('Clicked on anchor element');
+
+  // Prevent jumping to the section (default behaviour)
+  event.preventDefault();
+
+  // Get anchor element hash
+  var element = event.currentTarget,
+      hash = element.hash;
+
+  // Scroll the body and HTML to the ID equal to hash
+  $('html, body').animate({
+
+    // Find the offset of the targeted element on the page
+    scrollTop: $(hash).offset().top
+  }, 800, function(){
+
+    // Set URL to clicked hash
+    window.location.hash = hash;
+
+  });
+
+}
+
+/*
+**  Bind events
+*/
+$('a').on('click', scrollToSection);
